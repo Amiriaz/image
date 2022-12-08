4075b11ac94b9e04117a8f308e1dab2f44297418
Amir Riaz <116000783+Amiriaz@users.noreply.github.com>
2022-12-08 22:37:31 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,19 @@
+function abc(a){
+    console.log(a.src)
+    a.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2-DDz7RrlXm2MJGsIStgBlZUt5CpAptBUCA&usqp=CAU';
+}
+
+function xyz(a){
+    a.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-pU5cI_g3O-g3InbHOnRWt-gjQpcAmgKDeA&usqp=CAU";
+
+}
+
+function ab(a){
+    console.log(a.src)
+    a.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1m0eWvNQINsQ6_SLgYga8c_KdkcaxtOcRTg&usqp=CAU';
+}
+
+function xy(a){
+    a.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNE5tKU_XLTrhxnkA49_iEbDklFxwNkpc4rQ&usqp=CAU";
+
+}
